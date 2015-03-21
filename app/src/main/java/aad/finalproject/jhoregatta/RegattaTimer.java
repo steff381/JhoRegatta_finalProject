446c7601b43f6f0a91f9905e76feab0b7eb6571f
steff381 <daniel.steffen.analytics@gmail.com>
2015-03-21 10:25:33 -0500

    Tracker Changes
    Added master pause and start buttons and the code for onclicks of those
    buttons

--- app/src/main/java/aad/finalproject/jhoregatta/RegattaTimer.java
+++ app/src/main/java/aad/finalproject/jhoregatta/RegattaTimer.java
@@ -1,19 +1,58 @@
 package aad.finalproject.jhoregatta;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextClock;
+
+import java.util.Calendar;
 
 
 public class RegattaTimer extends SelectBoats {
+
+    public Button masterStart;
+    public Button masterPause;
+    public TextClock masterTimeClock;
+    public Calendar masterStartTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regatta_timer);
+
+        masterStart = (Button) findViewById(R.id.btnMasterStart);
+        masterPause = (Button) findViewById(R.id.btnMasterPause);
+        masterPause.setVisibility(View.INVISIBLE);
     }
 
+
+    public void startPauseSwitch() {
+        masterStart.setVisibility(View.INVISIBLE);
+        masterPause.setVisibility(View.VISIBLE);
+
+    }
+
+    public void pauseResumeSwitch() {
+        masterPause.setVisibility(View.INVISIBLE);
+        masterStart.setVisibility(View.VISIBLE);
+
+    }
+
+    public void onClickMasterStart(View view) {
+        startPauseSwitch();
+        Calendar cal = Calendar.getInstance();
+        Log.i("LogTag: TimeTracker ", "Calendar's toString is " + cal);
+
+
+    }
+
+
+    public void onClickMasterPause(View view) {
+        pauseResumeSwitch();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
